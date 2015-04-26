64735d7479a69eafb8f633a315e9ea1bf3db9f52
Izzat Bahadirov <twosheds@two-sheds.com>
2015-04-26 18:50:50 -0400

    Stopping calculation thread when Monte-Carlo activity is destroyed.

--- app/src/main/java/com/twosheds/pi/MonteCarloActivity.java
+++ app/src/main/java/com/twosheds/pi/MonteCarloActivity.java
@@ -64,6 +64,12 @@ public class MonteCarloActivity extends ActionBarActivity {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        isRunning = false;
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
