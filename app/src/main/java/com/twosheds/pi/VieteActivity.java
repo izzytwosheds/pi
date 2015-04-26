21490e1ce60dcb62f1299c697c5094f71f054254
Izzat Bahadirov <twosheds@two-sheds.com>
2015-04-26 19:19:40 -0400

    Implemented Viete product

---
+++ app/src/main/java/com/twosheds/pi/VieteActivity.java
@@ -0,0 +1,24 @@
+package com.twosheds.pi;
+
+public class VieteActivity extends PiSeriesActivity {
+    private double product = 1.0;
+
+    @Override
+    protected void init() {
+        piInit = 1;
+        formulaView.setImageResource(R.drawable.viete);
+    }
+
+    @Override
+    protected double calculatePi(double oldPi) {
+        double productTerm = Math.sqrt(2);
+        for (int i=0; i<numSteps; i++) {
+            productTerm = Math.sqrt(2 + productTerm);
+        }
+        productTerm /= 2;
+
+        product *= productTerm;
+
+        return 2 / product;
+    }
+}
